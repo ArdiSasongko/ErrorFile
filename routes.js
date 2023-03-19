be479b7a650216774d8bb802d4523f15ef074e0b
Ardi Sasongko <72727802+ArdiSasongko@users.noreply.github.com>
2023-03-20 00:11:47 +0700

    Add files via upload

--- routes.js
+++ routes.js
@@ -29,4 +29,4 @@ const routes = [
     }
 ]
 
-//module.exports =  {routes};
+module.exports =  {routes};
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const init = async () => {
         host: 'localhost',
     });
 
-    server.route()
+    server.route(routes)
     await server.start();
     console.log(`Server berjalan pada ${server.info.uri}`);
 };
